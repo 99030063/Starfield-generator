6b3ed380a2e2858e7a3a73792ccfaa35621d641b
Wesley <99030063@mydavinci.nl>
2019-10-29 11:45:43 +0100

    filled in circle, added second circle

--- canvas.js
+++ canvas.js
@@ -4,58 +4,53 @@ canvas.width = window.innerWidth;
 canvas.height= window.innerHeight;
 
 var c = canvas.getContext("2d") //c is context
-
-// c.fillStyle = "rgba( 0, 0, 255, 0.5)";
-// c.fillRect(100, 100, 100, 100);
-
-// console.log(canvas)
-
-//line
-// c.beginPath();
-// c.moveTo(50, 300);
-// c.lineTo(300, 100);
-// c.lineTo(400,300);
-// c.strokeStyle = "blue";
-// c.stroke();
-
-//arc / circle 
-// c.beginPath();
-// c.arc(300, 300, 30, 0, 2 * Math.PI);
-// c.strokeStyle = "yellow";
-// c.stroke();
-
-// for(var i = 0; i < 1000; i++){
-//     var x = Math.random() * window.innerWidth;
-//     var y = Math.random() * window.innerHeight;
-//     c.beginPath();
-//     c.arc(x, y, 30, 0, 2 * Math.PI);
-//     c.strokeStyle = "yellow";
-//     c.stroke();
-// }
     c.beginPath();
     c.arc(200, 200, 30, 0, 2 * Math.PI);
     c.strokeStyle = "yellow";
     c.stroke();
 
+
+
+    function Circle(x, y){
+        this.x = x;
+        this.y = y;
+
+        this.draw = function(){
+            c.beginPath();
+            c.arc(x, y, radius, 0, 2 * Math.PI);
+            c.fillStyle = "yellow";
+            c.fill();
+            c.strokeStyle = "yellow";
+            c.stroke();
+        }
+    }
+    
+    var circle = new Circle(200, 200);
+
     var x = Math.random() * innerWidth;
     var y = Math.random() * innerHeight;
-    var dx = Math.random() - 0.5;
-    var dy = 5;
+    var dx = (Math.random() - 0.5) * 5;
+    var dy = (Math.random() - 0.5) * 5;
     var radius = 30
 
     function animate(){
         requestAnimationFrame(animate);
         c.clearRect(0, 0, innerWidth, innerHeight);
+
+        circle.draw();
+        
         c.beginPath();
         c.arc(x, y, radius, 0, 2 * Math.PI);
+        c.fillStyle = "yellow";
+        c.fill();
         c.strokeStyle = "yellow";
         c.stroke();
 
-        if(x + radius > innerWidth | x -radius < 0){
+        if(x + radius > canvas.width | x -radius < 0){
             dx = -dx
         }
 
-        if(y + radius > innerHeight | y - radius < 0){
+        if(y + radius > canvas.height | y - radius < 0){
             dy = -dy
         }
         x += dx
